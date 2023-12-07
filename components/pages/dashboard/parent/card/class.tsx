b4b98152aae597701a8c6ce0be81cc774de7b384
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-07 14:42:15 +0800

    bug 9

--- components/pages/dashboard/parent/card/class.tsx
+++ components/pages/dashboard/parent/card/class.tsx
@@ -71,7 +71,7 @@ export function ClassCard({
             {classCourse?.class}
           </h3>
           <p className="text-xs text-muted-foreground">
-            {classCourse?.repeatedDays?.join(", ")}
+            {/* {classCourse?.repeatedDays?.join(", ")} */}
           </p>
           <div className="flex items-center justify-start gap-2">
             <div className="p-1 rounded-full w-7 h-7 bg-main-50">
