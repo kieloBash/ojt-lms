62468d8ea60d0a3d14c2ce0ce4fd28e737a1e073
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-07 10:59:59 +0800

    bug 4

---
+++ app/(pages)/(root)/auth/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import Temp from "@/components/pages/home/temp";
+import React from "react";
+
+const SignUpPage = async () => {
+  return (
+    <main className="">
+      <Temp />
+    </main>
+  );
+};
+
+export default SignUpPage;
