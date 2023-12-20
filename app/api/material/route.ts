158428c2aff13a38b4d42369fff07e8813105b71
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-20 13:12:45 +0800

    bug 2

--- app/api/material/route.ts
+++ app/api/material/route.ts
@@ -6,15 +6,15 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     connectDB();
-    const params = req.url.split("?");
+    // const params = req.url.split("?");
     let page: number = 1;
-    for (let i = 0; i < params.length; i++) {
-      const param = params[i].split("=");
-      if (param[0] === "page") {
-        page = Number(param[1]);
-        break;
-      }
-    }
+    // for (let i = 0; i < params.length; i++) {
+    //   const param = params[i].split("=");
+    //   if (param[0] === "page") {
+    //     page = Number(param[1]);
+    //     break;
+    //   }
+    // }
 
     const limit = pageLimit;
     const skip = (page - 1) * limit;
