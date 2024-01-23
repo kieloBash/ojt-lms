85e8c1a490a428a8bd79873d1c4e95e5e6872bd6
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2024-01-23 21:46:16 +0800

    bug 1

--- components/pages/dashboard/help/intro.tsx
+++ components/pages/dashboard/help/intro.tsx
@@ -8,8 +8,8 @@ const IntroComponent = () => {
     <Card className="flex flex-col p-4">
       <h2 className="text-2xl font-semibold">Let's Get Started!</h2>
       <p className="text-sm text-muted-foreground">
-        Welcome to our new Umonics Method Learning System! Here is a quick guide to get you
-        started. Follow the video below.
+        {`Welcome to our new Umonics Method Learning System! Here is a quick guide to get you
+        started. Follow the video below.`}
       </p>
       <div className="grid w-full grid-cols-5 gap-8 mt-6">
         <div className="col-span-2">
@@ -31,29 +31,29 @@ const IntroComponent = () => {
           </h3>
           <ul className="flex flex-col gap-1 mt-2 ml-4 list-decimal">
             <li className="">
-              Go to the <span className="font-medium">"Calendar"</span> tab
+              Go to the <span className="font-medium">{`"Calendar"`}</span> tab
               located on the left side.
             </li>
             <li className="">
               From there you can click the{" "}
-              <span className="font-medium">"Add Class"</span> button located on
-              the left hand side.
+              <span className="font-medium">{`"Add Class"`}</span> button
+              located on the left hand side.
               <p className="text-xs text-muted-foreground">
-                *Note that you can only schedule one class per week and a day
+                {`*Note that you can only schedule one class per week and a day
                 before the actual class. The start of the week is every Saturday
-                and the end is every Friday.
+                and the end is every Friday.`}
               </p>
             </li>
             <li className="">
-              There will be notifications in case you haven't enrolled for a
-              class that week.
+              {`There will be notifications in case you haven't enrolled for a
+              class that week.`}
               <p className="text-xs text-muted-foreground">
-                *Note that we don't conduct make-up class in the event you
-                missed enrolling for a class that week.
+                {`*Note that we don't conduct make-up class in the event you
+                missed enrolling for a class that week.`}
               </p>
             </li>
             <li className="">
-              Enjoy and welcome to the Umonics Method family!
+              {`Enjoy and welcome to the Umonics Method family!`}
             </li>
           </ul>
         </div>
