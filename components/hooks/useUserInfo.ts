aff93173133dc585c84466c858f52c372e5d6835
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2024-01-23 16:14:57 +0800

    bug 1

--- components/hooks/useUserInfo.ts
+++ components/hooks/useUserInfo.ts
@@ -2,37 +2,15 @@
 
 import { fetchSingleParentClerkId } from "@/lib/actions/parent.action";
 import { ParentType } from "@/lib/interfaces/parent.interface";
-import { UserType } from "@/lib/interfaces/user.interface";
 import { useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 const useUserInfo = () => {
-  // const [userInfo, setUserInfo] = useState<ParentType | UserType>();;
-
-  // useEffect(() => {
-  //   async function getParent() {
-  //     if (!user || !isLoaded) return null;
-  //     const parentInfo = await fetchSingleParentClerkId({
-  //       clerkId: user.id,
-  //     });
-  //     console.log(parentInfo);
-  //     return parentInfo;
-  //   }
-
-  //   if (user && !userInfo) {
-  //     getParent().then((res) => {
-  //       if (res) {
-  //         setUserInfo(res);
-  //       }
-  //     });
-  //   }
-  // }, [user, userInfo]);
+  const { user, isLoaded } = useUser();
 
   const { data, isLoading } = useQuery({
     queryKey: [`userInfo`],
     queryFn: async () => {
-      const { user, isLoaded } = useUser();
       console.log(user);
       if (!user) return null;
       console.log(user);
@@ -43,7 +21,7 @@ const useUserInfo = () => {
       });
 
       console.log(parentInfo);
-      return parentInfo;
+      return parentInfo as ParentType;
     },
   });
 
