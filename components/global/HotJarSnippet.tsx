466cfd14e3fd2b724b055a49bb044870cc07bd69
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2024-03-06 02:26:20 +0800

    tried hotjar 2 (npm)

--- components/global/HotJarSnippet.tsx
+++ components/global/HotJarSnippet.tsx
@@ -1,26 +1,19 @@
 "use client";
-import Script from "next/script";
-import React from "react";
+import Hotjar from "@hotjar/browser";
 
 const HotJarSnippet = () => {
-  const HOTJAR_ID = "3882899";
-  if (HOTJAR_ID) return null;
-  return (
-    <div>
-      <Script id="hotjar-snippet">
-        {`
-          (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:${HOTJAR_ID},hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-      </Script>
-    </div>
-  );
+  const hjid = 3882899;
+  const hjsv = 6;
+
+  const h = Hotjar.init(hjid, hjsv);
+
+  if (!h) return <></>;
+  // Initializing with `debug` option:
+  Hotjar.init(hjid, hjsv, {
+    debug: true,
+  });
+
+  return <></>;
 };
 
 export default HotJarSnippet;
